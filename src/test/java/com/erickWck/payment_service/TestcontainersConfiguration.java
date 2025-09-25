d9583f770cb5e36b7ad96d64a54ae0e6056321f9
Erick Nunes da Silva <149442208+erickknsilva@users.noreply.github.com>
2025-09-24 23:28:28 -0300

    Update TestcontainersConfiguration.java

--- src/test/java/com/erickWck/payment_service/TestcontainersConfiguration.java
+++ src/test/java/com/erickWck/payment_service/TestcontainersConfiguration.java
@@ -12,12 +12,6 @@ class TestcontainersConfiguration {
 
 	@Bean
 	@ServiceConnection
-	KafkaContainer kafkaContainer() {
-		return new KafkaContainer(DockerImageName.parse("apache/kafka-native:latest"));
-	}
-
-	@Bean
-	@ServiceConnection
 	PostgreSQLContainer<?> postgresContainer() {
 		return new PostgreSQLContainer<>(DockerImageName.parse("postgres:latest"));
 	}
